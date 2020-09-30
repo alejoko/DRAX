bf4a442defa46ed21ba950c149aa0f6ce3b49019
Carlos Mateo <cmc2k5@gmail.com>
2020-09-30 22:29:06 +0200

    Add UI menu icons

---
+++ UI/src/App/components/shared/Icons/index.tsx
@@ -0,0 +1,11 @@
+import MainMenuFavorite from './MainMenuFavorite';
+import MainMenuFolder from './MainMenuFolder';
+import MainMenuHelp from './MainMenuHelp';
+import MainMenuTarget from './MainMenuTarget';
+
+export {
+  MainMenuFavorite,
+  MainMenuFolder,
+  MainMenuHelp,
+  MainMenuTarget
+}
