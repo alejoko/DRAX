ea61e8c0fa68e25d9b13f4d91852b86641a06931
Isaura Ruiz <isaura.ruiz@amaris.com>
2020-10-06 09:37:55 +0200

    feat: add mocks  and converters

--- UI/src/modules/SetupCompany/components/CompanySummary/index.tsx
+++ UI/src/modules/SetupCompany/components/CompanySummary/index.tsx
@@ -11,12 +11,12 @@ type CompanySummaryProps = RouteComponentProps<{ lang: string }> & {
 };
 
 const companyInfo = {
-    companyId = '29218',
-    companyName = 'Drax Group',
-    companyDescription= 'Drax Group PLC is an electric utility company that produces energy for large customers and businesses in the United Kingdom. Drax generates thermal energy through the use of coal and biomass fuel sources. The majority of the power that the company produces comes from coal fuel sources. The rest comes from the burning of compressed wood pellets, or biomass, sourced from forests in the American South. Drax primarily brings in revenue through the sale of power. The sale of United Kingdom renewable certificates also represents a significant revenue stream. The company is one of the U.K. 's largest producers of renewable power and controls Europe's single largest decarburization project by way of its biomass facilities.',
-    companyIcon = FolderOpen,
-    domain = 'drax.com',
-    mainOffice = 'United Kingdom',
+    companyId: '29218',
+    companyName: 'Drax Group',
+    companyDescription : "Drax Group PLC is an electric utility company that produces energy for large customers and businesses in the United Kingdom. Drax generates thermal energy through the use of coal and biomass fuel sources. The majority of the power that the company produces comes from coal fuel sources. The rest comes from the burning of compressed wood pellets, or biomass, sourced from forests in the American South. Drax primarily brings in revenue through the sale of power. The sale of United Kingdom renewable certificates also represents a significant revenue stream. The company is one of the U.K. 's largest producers of renewable power and controls Europe's single largest decarburization project by way of its biomass facilities.",
+    companyIcon : FolderOpen,
+    domain : 'drax.com',
+    mainOffice : 'United Kingdom',
     employees: 666
 };
 
